065dc1ef6e84db102875fdffcc94632235786405
high9tech <hightechps96@gmail.com>
2026-07-27 18:45:08 +0300

    Add files via upload

--- payload_map.js
+++ payload_map.js
@@ -26,18 +26,31 @@ const payload_map = [
     //     fileName: "elfldr.elf",
     //     author: "john-tornblom",
     //     projectSource: "https://github.com/ps5-payload-dev/elfldr",
-    //     binarySource: "https://github.com/ps5-payload-dev/elfldr/releases/download/v0.19/Payload.zip",
-    //     version: "0.19",
+    //     binarySource: "https://github.com/ps5-payload-dev/pacbrew-repo/actions/runs/13686166926",
+    //     version: "0.18.1",
     //     supportedFirmwares: ["1.", "2.", "3.", "4.", "5."]
     // },
+    // etaHEN is added twice so that on 1.xx-2.xx you can load it in webkit only mode too
+    // but on 3.xx-4.xx it only shows in kernel exploit mode since it needs the 9020 elf loader for kstuff
     {
         displayTitle: "etaHEN",
         description: "AIO HEN",
-        fileName: "etaHEN-2.0b.bin",
+        fileName: "etaHEN.bin",
         author: "LightningMods, Buzzer, sleirsgoevy, ChendoChap, astrelsky, illusion, CTN, SiSTR0, Nomadic",
-        projectSource: "https://github.com/etaHEN/etaHEN",
-        binarySource: "https://github.com/etaHEN/etaHEN/raw/178499a10cf268093b048079e803717195fcab19/etaHEN-2.0b.bin",
-        version: "2.0b",
+        projectSource: "https://github.com/LightningMods/etaHEN",
+        binarySource: "https://github.com/LightningMods/etaHEN/releases/download/1.9b/etaHEN.bin",
+        version: "1.9b",
+        supportedFirmwares: ["3.", "4."]
+    },
+    {
+        displayTitle: "etaHEN",
+        description: "AIO HEN",
+        fileName: "etaHEN.bin",
+        author: "LightningMods, Buzzer, sleirsgoevy, ChendoChap, astrelsky, illusion, CTN, SiSTR0, Nomadic",
+        projectSource: "https://github.com/LightningMods/etaHEN",
+        binarySource: "https://github.com/LightningMods/etaHEN/releases/download/1.9b/etaHEN.bin",
+        version: "1.9b",
+        supportedFirmwares: ["1.", "2."],
         toPort: 9021
     },
     {
@@ -74,17 +87,17 @@ const payload_map = [
     },
     {
         displayTitle: "websrv",
-        description: "Custom homebrew loader. Runs on port 8080.",
+        description: "Uses john-tornblom's elfldr. Custom homebrew loader. Runs on port 8080.",
         fileName: "websrv.elf",
         author: "john-tornblom",
         projectSource: "https://github.com/ps5-payload-dev/websrv",
-        binarySource: "https://github.com/ps5-payload-dev/websrv/releases/download/v0.22/Payload.zip",
-        version: "0.22",
+        binarySource: "https://github.com/ps5-payload-dev/pacbrew-repo/actions/runs/13686166926",
+        version: "0.20",
         toPort: 9021
     },
     {
         displayTitle: "ftpsrv",
-        description: "FTP server. Runs on port 2121.",
+        description: "Uses john-tornblom's elfldr. FTP server. Runs on port 2121.",
         fileName: "ftpsrv.elf",
         author: "john-tornblom",
         projectSource: "https://github.com/ps5-payload-dev/ftpsrv",
@@ -94,7 +107,7 @@ const payload_map = [
     },
     {
         displayTitle: "klogsrv",
-        description: "Klog server. Runs on port 3232.",
+        description: "Uses john-tornblom's elfldr. Klog server. Runs on port 3232.",
         fileName: "klogsrv.elf",
         author: "john-tornblom",
         projectSource: "https://github.com/ps5-payload-dev/klogsrv",
@@ -104,7 +117,7 @@ const payload_map = [
     },
     {
         displayTitle: "shsrv",
-        description: "Telnet shell server. Runs on port 2323.",
+        description: "Uses john-tornblom's elfldr. Telnet shell server. Runs on port 2323.",
         fileName: "shsrv.elf",
         author: "john-tornblom",
         projectSource: "https://github.com/ps5-payload-dev/shsrv",
@@ -114,7 +127,7 @@ const payload_map = [
     },
     {
         displayTitle: "gdbsrv",
-        description: "GDB server. Runs on port 2159.",
+        description: "Uses john-tornblom's elfldr. GDB server. Runs on port 2159.",
         fileName: "gdbsrv.elf",
         author: "john-tornblom",
         projectSource: "https://github.com/ps5-payload-dev/gdbsrv",
